d9eb83e9ca162202f95873bbbebff454690ee92b
Santeri Moilanen <h9029@student.jamk.fi>
2017-02-05 20:12:23 +0200

    Tein puolet

--- src/looping_is_recursion.clj
+++ src/looping_is_recursion.clj
@@ -1,16 +1,37 @@
 (ns looping-is-recursion)
 
 (defn power [base exp]
-  ":(")
+  (let [helper (fn [ac base exp]
+                 (if (zero? exp)
+                   ac
+                   (recur (* ac base) base (dec exp))))]
+    (helper 1 base exp)))
 
 (defn last-element [a-seq]
-  ":(")
+  (cond
+   (empty? a-seq) nil
+    (= 1 (count a-seq)) (first a-seq)
+    :else (recur (rest a-seq))
+   )
+)
 
-(defn seq= [seq1 seq2]
-  ":(")
+(defn seq= [a-seq b-seq]
+  (cond
+  (and (empty? a-seq) (empty? b-seq)) true
+  (= a-seq b-seq) (recur (rest a-seq) (rest b-seq))
+   :else false
+  )
+)
 
 (defn find-first-index [pred a-seq]
-  ":(")
+  (loop [n 0
+         s a-seq]
+    (cond
+     (empty? s) nil
+     (pred (first s)) n
+     :else (recur (inc n) (rest s))
+    )
+))
 
 (defn avg [a-seq]
   -1)
