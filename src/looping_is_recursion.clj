f6f4eaad24fba4ea102fe92e0759c59e649755d0
Santeri Moilanen <h9029@student.jamk.fi>
2017-02-06 13:35:03 +0200

    all done

--- src/looping_is_recursion.clj
+++ src/looping_is_recursion.clj
@@ -34,14 +34,45 @@
 ))
 
 (defn avg [a-seq]
-  -1)
+  (loop [n 0
+         jako 0
+         s a-seq]
+    (cond
+      (empty? s) (/ n jako)
+     :else (recur (+ n (first s)) (inc jako) (rest s))))
+)
+
+(defn toggle [a-set elem]
+  (if (contains? a-set elem) (disj a-set elem) (conj a-set elem))
+)
 
 (defn parity [a-seq]
-  ":(")
+  (loop [stri #{}
+         s a-seq]
+    (cond
+      (empty? s) stri
+     :else (recur (toggle stri (first s))  (rest s))))
+)
 
 (defn fast-fibo [n]
-  ":(")
+  (loop [x1 0
+         x2 1
+         nt 2]
+    (cond
+     (= 1 n) 1
+     (= 0 n) 0
+     (= nt n) (+ x1 x2)
+     :else (recur x2 (+ x2 x1) (inc nt)))
+    )
+)
 
 (defn cut-at-repetition [a-seq]
-  [":("])
+(loop [st #{}
+         a-seq a-seq
+         acc []]
+      (cond
+        (empty? a-seq) acc
+        (st (first a-seq)) acc
+        :else (recur (conj st (first a-seq)) (rest a-seq) (conj acc (first a-seq)))
+)))
 
